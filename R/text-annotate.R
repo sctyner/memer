f5adc3218de41046e1dbfa9d1ea4992f47212c1b
Sam Tyner <sctyner@iastate.edu>
2019-01-23 22:04:41 -0600

    add distracted boyfriend function

--- R/text-annotate.R
+++ R/text-annotate.R
@@ -3,8 +3,9 @@
 #'
 #' get_meme("AllTheThings") %>% top_text("R ALL THE THINGS!")
 #' get_meme("OprahGiveaway") %>% bottom_text("EVERYONE GETS A %>%!!!")
+#' get_meme("DistractedBf") %>% distbf_text("tidyverse", "new R users", "base R")
 #'
-#' @importFrom magick image_annotate
+#' @importFrom magick image_annotate geometry_point
 #' @export
 #'
 top_text <- function(img, txt, ...){
@@ -19,4 +20,18 @@ bottom_text <- function(img, txt, ...){
   image_annotate(img, txt, font = "Impact",
                 color = "white", strokecolor = "black",
                 gravity = "South", size = 44, ...)
+}
+
+#' @export
+#'
+distbf_text <- function(img, newgirl, guy, oldgirl, ...){
+  image_annotate(img, newgirl, font = "Impact",
+                 color = "white", strokecolor = "black",
+                 gravity = "SouthWest", location = geometry_point(65,0), size = 44, ...) %>%
+    image_annotate(guy, font = "Impact",
+                   color = "white", strokecolor = "black",
+                   gravity = "Center", location = geometry_point(65,0), size = 44, ...) %>%
+    image_annotate(oldgirl, font = "Impact",
+                   color = "white", strokecolor = "black",
+                   gravity = "SouthEast", location = geometry_point(10, 30), size = 44, ...)
 }
